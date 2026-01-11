c2461f946f2bd838525608e0776654530e58256f
Amsatnaf <diego.alme@hotmail.com>
2026-01-11 22:47:28 +0000

    Alterei tudo, menos o .s2i

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-
-from flask import Flask, jsonify
+import os
+import psycopg2
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/healthz")
-def healthz():
-    return jsonify(status="ok"), 200
+def get_db_connection():
+    # Pega as variáveis que o OpenShift vai injetar
+    conn = psycopg2.connect(
+        host=os.environ.get('DB_HOST'),
+        database=os.environ.get('DB_NAME'),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD')
+    )
+    return conn
 
-@app.route("/")
-def root():
-    return jsonify(service="flask-hello", message="Hello OpenShift!"), 200
+@app.route('/')
+def hello():
+    db_status = "Desconectado..."
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()
+        cur.close()
+        conn.close()
+        db_status = f"Conectado com Sucesso! Versão: {db_version}"
+    except Exception as e:
+        db_status = f"Erro ao conectar no banco: {str(e)}"
+
+    return f"""
+    <h1>Aplicação Python no OpenShift</h1>
+    <p>Status do Banco: <b>{db_status}</b></p>
+    <p>Versão do App: 1.0 (Azul)</p>
+    """
 
 if __name__ == "__main__":
-    # S2I e OpenShift gostam da porta 8080
-    app.run(host="0.0.0.0", port=8080)
-
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
